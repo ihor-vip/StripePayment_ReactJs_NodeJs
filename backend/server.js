348e9e50d640f47526cfe31f222a3f3d8324e328
ihor-vip <vip20140902@gmail.com>
2023-05-24 17:03:57 +0300

    backend part

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,48 @@
-import express from 'express';
+require("dotenv").config()
+
+const express = require("express")
+const app = express()
+const cors = require("cors")
+
+
 const port = 8080;
+app.use(express.json());
 
-const app = express();
+app.use(
+    cors({
+        origin:"http://127.0.0.1:3000",
+    })
+)
 
-app.get('/', (req, res) => res.send('Server started'))
+const stripe = require("stripe")(process.env.STRIPE_PRIVATE_KEY)
+
+app.post("/create-checkout-session", async (req, res)=>{
+    try{
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            mode: "payment",
+            line_items: req.body.items.map(item => {
+                return{
+                    price_data: {
+                        currency: "usd",
+                        product_data: {
+                            name: item.name
+                        },
+                        unit_amount: ( item.price ) * 100,
+
+                    },
+                    quantity: item.quantity
+                }
+            }),
+            success_url: 'http://127.0.0.1:3000/success',
+            cancel_url: 'http://127.0.0.1:3000/cancel'
+        })
+
+        res.json({url: session.url})
+
+    }catch(e){
+        res.status(500).json({error:e.message})
+    }
+})
 
 app.listen(port,() => console.log(`Server running on port ${port}`))
